f54b8085c4f16cf805d2490ad06f8145683aa62b
eqperes <eqperes@gmail.com>
2020-01-09 20:54:36 -0300

    first test

---
+++ tests/test_image_to_pixels.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+from services import PkmnImageManager
+
+
+def test_image_to_pixels():
+    pkmn_image_manager = PkmnImageManager()
+    pixel_array = pkmn_image_manager.get_pkmn_image_pixel_array(pkmn_nb=16)
+    assert isinstance(pixel_array, np.array)
+    assert isinstance(pixel_array.shape[1], 3)
