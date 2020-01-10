ccba39fdaf5780533910274bbd52e8673ceaffcd
renanr <renan.roveri@gmail.com>
2020-01-09 21:39:39 -0300

    Declares properties and Generation ENUM

--- domain/pokemon.py
+++ domain/pokemon.py
@@ -1,4 +1,20 @@
+from enum import Enum
+from os.path import abspath
+
+
+class Gen(Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+
+
 class Pokemon:
+    number: int
+    generation: Gen
+    image_path: str
+
     @classmethod
     def do_we_gotta_catch_em_all(cls) -> bool:
         return True
