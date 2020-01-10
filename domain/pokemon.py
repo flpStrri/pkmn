10713ac4f660898cd4f7e88ee3cee7614526f03f
renanr <renan.roveri@gmail.com>
2020-01-09 21:40:00 -0300

    Implements getter for image path for a given pokemon

--- domain/pokemon.py
+++ domain/pokemon.py
@@ -18,3 +18,12 @@ class Pokemon:
     @classmethod
     def do_we_gotta_catch_em_all(cls) -> bool:
         return True
+
+    @staticmethod
+    def get_image_path(number: int, back: bool = False, female: bool = False, shiny: bool = False) -> str:
+        path = ""
+        path = path + "/back" if back else path
+        path = path + "/shiny" if shiny else path
+        path = path + "/female" if female else path
+        path = path + "/" + str(number) + ".png"
+        return abspath(path)
