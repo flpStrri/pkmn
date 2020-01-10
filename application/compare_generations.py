80b3b0ea6cef01e7c11b7cf419498a1d379547fc
renanr <renan.roveri@gmail.com>
2020-01-10 00:00:05 -0300

    fixes range

--- application/compare_generations.py
+++ application/compare_generations.py
@@ -3,7 +3,7 @@ from service.pkmn_image_manager import PkmnImageManager
 
 
 def calculate_gen_one():
-    pokemons = [Pokemon(i) for i in range(151)]
+    pokemons = [Pokemon(i) for i in range(1, 151)]
     paths = [pokemon.image_path for pokemon in pokemons]
     pkmn_color_complexities = [PkmnImageManager.get_complexity_from_path(path) for path in paths]
     for pokemon in pokemons:
