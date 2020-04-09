56e1720ad40784ccdf121438a74bed3d2e83a76e
eqperes <eqperes@gmail.com>
2020-04-09 23:23:49 +0200

    update script with more details.

--- application/compare_generations.py
+++ application/compare_generations.py
@@ -3,12 +3,15 @@ from service.pkmn_image_manager import PkmnImageManager
 
 
 def calculate_gen_one():
-    pokemons = [Pokemon(i) for i in range(151)]
-    paths = [pokemon.image_path for pokemon in pokemons]
-    pkmn_color_complexities = [PkmnImageManager.get_complexity_from_path(path) for path in paths]
+    pokemons = [Pokemon(i) for i in range(1, 151)]
+    pkmn_color_complexities = []
+    for i, pokemon in enumerate(pokemons):
+        print(i, pokemon.name)
+        pkmn_color_complexities.append(PkmnImageManager.get_complexity_from_path(pokemon.image_path, 8))
     for pokemon in pokemons:
         print(f"Pokemon: {pokemon.name}")
         print(f"Color complexity: {pkmn_color_complexities[pokemon.number - 1]}")
+    return pkmn_color_complexities
 
 
 if __name__ == "__main__":
